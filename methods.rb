89a1cc5b183e925cb98eb048318509ade48a5106
briankorir44 <briankorir44@yahoo.com>
2022-10-18 20:02:38 +0300

    complete lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+def greet(name="Naureen")
+    puts"Hello, #{name}!"
+    # puts"Hello, #{name_2}"
+    end
+    greet
+# # def greet_with_default name=""
+#     puts "Hello, #{name}!"
+#     end
+    # greet_with_default
+    def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+    end
+    greet_with_default
+
+    def add num1=2,num2=5
+        return num1 + num2
+        end
+add
+       def halve num=7
+        if num.class != Integer
+            return nil
+          end
+        return num/2
+        end
+        halve
